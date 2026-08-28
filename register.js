414190ef27efddd8bec62d7b90b1444801651443
Minerva <128452264+minaaris@users.noreply.github.com>
2026-08-28 22:26:12 +1000

    some js form validation

--- register.js
+++ register.js
@@ -1,9 +1,40 @@
+//buttons
 const submitButton = document.getElementById("submitButton");
 const cancelButton = document.getElementById("cancelButton");
 const successModal = document.getElementById("successModal");
-const closeButton = document.getElementById("closeButton")
+const closeButton = document.getElementById("closeButton");
+
+//form
+const firstName = document.getElementById("firstName");
+const lastName = document.getElementById("lastName");
+const email = document.getElementById("email");
+const errorText = document.getElementById("errorText");
+
+
 submitButton.onclick = function() {
-    successModal.style.display = "block";
+    let valid = true;
+    if (firstName.value.trim() == "") {
+        valid = false;
+        errorText.innerHTML = "missing information";
+    } else {
+        valid = true;
+    }
+    if (lastName.value.trim() == "") {
+        valid = false;
+        errorText.innerHTML = "missing information";
+    } else {
+        valid = true;
+    }
+    if (email.value.trim() == "") {
+        valid = false;
+        errorText.innerHTML = "missing information";
+    } else {
+        valid = true;
+    }
+    if (valid == true) {
+        successModal.style.display = "block";
+    }
+    event.preventDefault();
 }
 closeButton.onclick = function() {
     successModal.style.display = "none";
